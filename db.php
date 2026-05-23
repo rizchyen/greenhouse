8d55fb3920042c7ee1e409dbc00f568ba01248c5
rizchyen <yenyenrizchelle@gmail.com>
2026-05-23 13:14:34 +0800

    Create db.php

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+
+$conn = new mysqli(
+    getenv('MYSQLHOST'),
+    getenv('MYSQLUSER'),
+    getenv('MYSQLPASSWORD'),
+    getenv('MYSQLDATABASE'),
+    getenv('MYSQLPORT')
+);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+?>
